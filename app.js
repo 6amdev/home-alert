e188586aa4a91d903d9ba4b59937bca61b4129c6
6amdev <121995284+6amdev@users.noreply.github.com>
2023-03-02 15:31:46 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const system_tools = require('./helpers/system-tools');
+const line_notify = require('./helpers/line-notify');
+
+system_tools.cronjobM(1);
+line_notify.sendMessage("ทดสอบส่งข้อความ");
+
+const test_function = async () => {
+    let public_ip = await system_tools.publicIp();
+    console.log(`Public IP address: ${public_ip}`);
+}
+test_function();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
